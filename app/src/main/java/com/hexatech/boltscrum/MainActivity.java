5ee9f4767aa6ab373a0a4f765af0ae387540bf3a
Camille Pignon <camille.dourferpignon@gmail.com>
2018-03-10 18:17:06 +0100

    Modified the TeamFragment to display a list

--- app/src/main/java/com/hexatech/boltscrum/MainActivity.java
+++ app/src/main/java/com/hexatech/boltscrum/MainActivity.java
@@ -6,6 +6,12 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,15 +25,15 @@ public class MainActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     HomeFragment home = new HomeFragment();
-                    fragmentManager.beginTransaction().replace(R.id.fragmentContainer, home).commit();
+                    fragmentManager.beginTransaction().replace(R.id.container, home).commit();
                     return true;
                 case R.id.navigation_team:
                     TeamFragment team = new TeamFragment();
-                    fragmentManager.beginTransaction().replace(R.id.fragmentContainer, team).commit();
+                    fragmentManager.beginTransaction().replace(R.id.container, team).commit();
                     return true;
                 case R.id.navigation_parameters:
                     ParametersFragment parameters = new ParametersFragment();
-                    fragmentManager.beginTransaction().replace(R.id.fragmentContainer, parameters).commit();
+                    fragmentManager.beginTransaction().replace(R.id.container, parameters).commit();
                     return true;
             }
             return false;
@@ -41,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
 
         this.fragmentManager = getSupportFragmentManager();
         HomeFragment home = new HomeFragment();
-        fragmentManager.beginTransaction().replace(R.id.fragmentContainer, home).commit();
+        fragmentManager.beginTransaction().add(R.id.container, home).commit();
 
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
--- app/src/main/java/com/hexatech/boltscrum/TeamFragment.java
+++ app/src/main/java/com/hexatech/boltscrum/TeamFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 /**
@@ -13,17 +15,27 @@ import android.view.ViewGroup;
  */
 public class TeamFragment extends Fragment {
 
-
     public TeamFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_team, container, false);
+        View view = inflater.inflate(R.layout.fragment_team, container, false);
+
+        String[] members = new String[] { "Camille PIGNON", "Benjamin LECLERC" };
+        ListView listView = view.findViewById(R.id.membersList);
+
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                members);
+
+        listView.setAdapter(listViewAdapter);
+
+        return view;
     }
 
 }
