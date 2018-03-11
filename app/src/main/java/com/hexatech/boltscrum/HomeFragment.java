2d7dd9b0e57b88c2132f840d2388659ddb8b999d
Camille Pignon <camille.dourferpignon@gmail.com>
2018-03-11 09:31:25 +0100

    Added the MeetingActivity and designed its layout

--- app/src/main/java/com/hexatech/boltscrum/HomeFragment.java
+++ app/src/main/java/com/hexatech/boltscrum/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.hexatech.boltscrum;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -19,11 +20,22 @@ public class HomeFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private View.OnClickListener mOnStartMeetingButtonClickedListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(getActivity(), MeetingActivity.class);
+            startActivity(intent);
+        }
+    };
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        Button startMeetingButton = view.findViewById(R.id.startMeetingButton);
+        startMeetingButton.setOnClickListener(mOnStartMeetingButtonClickedListener);
 
         // Inflate the layout for this fragment
         return view;
---
+++ app/src/main/java/com/hexatech/boltscrum/MeetingActivity.java
@@ -0,0 +1,13 @@
+package com.hexatech.boltscrum;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MeetingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meeting);
+    }
+}
